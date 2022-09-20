12073a7827d74827a197d2a03792b10cb24509fa
Sadyk Nasrullaev <sadik.nasrullaev@raiffeisen.ru>
2022-09-20 18:40:38 +0300

    Version catalog support added. Common dependencies added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,18 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(deps.plugins.android.application)
+    alias(deps.plugins.kotlin.android)
 }
 
 android {
-    namespace = "mon.ie.android"
-    compileSdk = 33
+    namespace = config.versions.appId.get()
+    compileSdk = config.versions.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = "mon.ie.android"
-        minSdk = 26
-        targetSdk = 33
-        versionCode = 1
-        versionName = "0.0.1"
+        applicationId = config.versions.appId.get()
+        minSdk = config.versions.minSdk.get().toInt()
+        targetSdk = config.versions.targetSdk.get().toInt()
+        versionCode = config.versions.versionCode.get().toInt()
+        versionName = config.versions.versionName.get()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -26,33 +26,32 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
+
     buildFeatures {
         compose = true
     }
+
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.3.1"
+        kotlinCompilerExtensionVersion = deps.versions.compose.compilerVersion.get()
     }
+
     packagingOptions {
         resources.excludes.add("/META-INF/{AL2.0,LGPL2.1}")
     }
 }
 
 dependencies {
-
-    implementation("androidx.compose.ui:ui:1.2.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.2.1")
-    implementation("androidx.compose.material:material:1.2.1")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.2.1")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.2.1")
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
-    implementation("androidx.activity:activity-compose:1.5.1")
+    implementation(dependencyNotation = deps.bundles.compose)
+    implementation(dependencyNotation = deps.compose.activity)
+    implementation(dependencyNotation = deps.coreKtx)
+    debugImplementation(dependencyNotation = deps.compose.ui.tooling)
 }
