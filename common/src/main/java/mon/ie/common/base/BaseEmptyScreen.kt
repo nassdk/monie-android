2802416a639157b4bd6aebc95cff7c4b7b415f76
Sadyk Nasrullaev <nassdk17@yandex-team.ru>
2022-10-16 23:46:19 +0300

    Additions to base empty cores.

--- common/src/main/java/mon/ie/common/base/BaseEmptyScreen.kt
+++
@@ -1,7 +0,0 @@
-package mon.ie.common.base
-
-import mon.ie.common.base.empty.EmptyViewModel
-
-abstract class BaseEmptyScreen : BaseScreen<BaseScreenState, BaseScreenEvent, BaseVmCommand>() {
-  override val viewModel = EmptyViewModel.INSTANCE
-}
---
+++ common/src/main/java/mon/ie/common/base/empty/EmptyScreenState.kt
@@ -0,0 +1,5 @@
+package mon.ie.common.base.empty
+
+import mon.ie.common.base.BaseScreenState
+
+class EmptyScreenState internal constructor() : BaseScreenState
