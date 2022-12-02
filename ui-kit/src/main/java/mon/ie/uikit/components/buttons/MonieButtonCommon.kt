36144241c5b86c68203d58c1042845450d3dc9c2
Sadyk Nasrullaev <nassdk17@yandex-team.ru>
2022-12-03 00:47:34 +0300

    Click animation modifier added.

--- ui-kit/src/main/java/mon/ie/uikit/components/buttons/MonieButtonCommon.kt
+++ ui-kit/src/main/java/mon/ie/uikit/components/buttons/MonieButtonCommon.kt
@@ -17,45 +17,52 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import mon.ie.uikit.helpers.animatedOnClick
 import mon.ie.uikit.helpers.button.MonieButtonColors
 import mon.ie.uikit.helpers.button.MonieButtonShape
 import mon.ie.uikit.helpers.button.MonieButtonShape.LARGE
 
+private const val BUTTON_CLICK_SCALE_FACTORY = .95f
+
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun MonieButtonCommon(
-  onClick: () -> Unit,
-  modifier: Modifier = Modifier,
-  shape: MonieButtonShape = LARGE,
-  enabled: Boolean = true,
-  colors: MonieButtonColors,
-  content: @Composable RowScope.() -> Unit
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    shape: MonieButtonShape = LARGE,
+    enabled: Boolean = true,
+    colors: MonieButtonColors,
+    content: @Composable RowScope.() -> Unit
 ) {
 
-  val backgroundColor by colors.backgroundColor(isEnabled = enabled)
+    val backgroundColor by colors.backgroundColor(isEnabled = enabled)
+    val interactionSource = remember { MutableInteractionSource() }
 
-  Surface(
-    onClick = onClick,
-    modifier = modifier,
-    enabled = enabled,
-    shape = shape.value.invoke(),
-    color = backgroundColor,
-    contentColor = backgroundColor,
-    elevation = 0.dp,
-    interactionSource = remember { MutableInteractionSource() },
-    content = {
-      CompositionLocalProvider(LocalContentAlpha provides backgroundColor.alpha) {
-        ProvideTextStyle(
-          value = MaterialTheme.typography.button,
-          content = {
-            Row(
-              modifier = Modifier.padding(ButtonDefaults.ContentPadding),
-              verticalAlignment = Alignment.CenterVertically,
-              content = content
-            )
-          }
-        )
-      }
-    }
-  )
+    Surface(
+        onClick = onClick,
+        modifier = modifier.animatedOnClick(
+            interactionSource = interactionSource,
+            scaleDownFactor = BUTTON_CLICK_SCALE_FACTORY
+        ),
+        enabled = enabled,
+        shape = shape.value.invoke(),
+        color = backgroundColor,
+        contentColor = backgroundColor,
+        elevation = 0.dp,
+        interactionSource = interactionSource,
+        content = {
+            CompositionLocalProvider(LocalContentAlpha provides backgroundColor.alpha) {
+                ProvideTextStyle(
+                    value = MaterialTheme.typography.button,
+                    content = {
+                        Row(
+                            modifier = Modifier.padding(ButtonDefaults.ContentPadding),
+                            verticalAlignment = Alignment.CenterVertically,
+                            content = content
+                        )
+                    }
+                )
+            }
+        }
+    )
 }
