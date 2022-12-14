d7ea8588660660e60102b1edd74b06ff42cdca60
Sadyk Nasrullaev <nassdk17@yandex-team.ru>
2022-12-14 20:55:51 +0300

    Pre-push script minors.

--- config/githooks/pre-push.sh
+++ config/githooks/pre-push.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Запускаем анализатор кода детект..."
+echo "Launching detekt"
 
 # Inspect code using Detekt
 bash gradlew detekt
@@ -8,9 +8,7 @@ bash gradlew detekt
 status=$?
 
 if [ "$status" = 0 ] ; then
-    echo "Анализатор кода закончил проверку. Хорошая работа:)"
     exit 0
 else
-    echo 1>&2 "Анализатор кода нашел нарушения. Поправь их пожалуйста!"
     exit 1
 fi
