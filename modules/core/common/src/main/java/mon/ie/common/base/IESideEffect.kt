6a7ca68f98bcacba73d8a0676db932351b80df94
Sadyk Nasrullaev <nassdk17@yandex-team.ru>
2023-01-02 18:36:58 +0300

    Viewmodel side effects added.

--- modules/core/common/src/main/java/mon/ie/common/base/IESideEffect.kt
+++ modules/core/common/src/main/java/mon/ie/common/base/IESideEffect.kt
@@ -1,3 +1,9 @@
 package mon.ie.common.base
 
-interface IESideEffect
+data class IESideEffect(val isTriggered: Boolean) {
+    companion object {
+        fun IESideEffect.consumed(): IESideEffect = IESideEffect(isTriggered = false)
+        fun IESideEffect.triggered(): IESideEffect = IESideEffect(isTriggered = true)
+        fun initial(): IESideEffect = IESideEffect(isTriggered = false)
+    }
+}
